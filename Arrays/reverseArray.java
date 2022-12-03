edacdd945ab172bdfbee6ba595d1ad4c5ac5c0a7
gnanasai5111 <56872239+gnanasai5111@users.noreply.github.com>
2022-12-03 10:50:28 +0530

    Update reverseArray.java

--- Arrays/reverseArray.java
+++ Arrays/reverseArray.java
@@ -19,3 +19,30 @@
 (Since you are looping through the entire array).
 -> Space Complexity - o(n)
  (Since we used the temporary array to store the result). 
+  
+**Second Approach**
+
+->We Can further optimise this code by not using the temorary array and doing in place shifting in array.
+  
+  
+  class Reverse {
+    public static void main(String[] args) {
+        int arr[] = {1, 4,5,2, 3};
+       int start=0;
+       int end=arr.length-1;
+       while(start<end){
+           int temp=arr[start];
+           arr[start]=arr[end];
+           arr[end]=temp;
+           start++;
+           end--;
+       }
+        System.out.println(Arrays.toString(arr));
+        
+    }
+}
+
+-> Time Complexity - o(n) 
+(Since you are looping through the entire array).
+-> Space Complexity - o(1)
+ (Since we did it in in-place). 
