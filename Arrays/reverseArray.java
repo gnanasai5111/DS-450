295c74685177a5e99e09e486cc38774354120288
gnanasai5111 <56872239+gnanasai5111@users.noreply.github.com>
2022-12-03 10:42:31 +0530

    Update reverseArray.java

--- Arrays/reverseArray.java
+++ Arrays/reverseArray.java
@@ -1,1 +1,21 @@
 
+**First Approach**
+
+->Create an temporary array and copy the contents of one array to other array .
+  
+  class Reverse {
+    public static void main(String[] args) {
+        int arr[] = {1, 4,2, 3};
+        int res[]=new int[arr.length];
+        for(int i=0;i<res.length;i++){
+            res[i]=arr[arr.length-1-i];
+        }
+        System.out.println(Arrays.toString(res));
+        
+    }
+}
+
+-> Time Complexity - o(n) 
+(Since you are looping through the entire array).
+-> Space Complexity - o(n)
+ (Since we used the temporary array to store the result). 
